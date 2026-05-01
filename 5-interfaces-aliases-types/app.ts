813385df837284ea25529f966d7d31d1bce93441
tahin <mdtahinhassanjihad@gmail.com>
2026-05-01 17:36:18 +0600

    interface and alias use method

---
+++ 5-interfaces-aliases-types/app.ts
@@ -0,0 +1,32 @@
+      //!  interface and alias use wey
+
+//? 1. designing public APIs : interface
+//? 2. for simple object : use both ja kono akta
+//? 3. union and intersection types: use type
+//? 4. primitive or functions : use type
+//? 5. flexibility : interface(extends) 
+
+
+//!  alias type 
+type User = {
+    name: string,
+    age: number,
+    email: string
+}
+
+
+//!  define interface type
+//* interface data merge kola jay / data jok kola jay
+interface UserProfile {
+    name: string,
+    age: number,
+    email: string
+}
+
+//? marge object 
+interface Person{
+    name:string
+}
+interface Person{
+    age:number
+}
